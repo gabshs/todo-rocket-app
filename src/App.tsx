b3a7d81c0f11c17b0e694bf27b61c27022e632c0
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-01-28 03:30:11 -0300

    feat: create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,22 @@
+import Logo from "./assets/logo.svg";
+import { PlusCircle } from "phosphor-react";
+
+import styles from "./App.module.css";
+
 export function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <header className={styles.header}>
+        <img src={Logo} alt="todo logo" />
+      </header>
+      <main className={styles.container}>
+        <form className={styles.taskForm}>
+          <input type="text" placeholder="Adicione uma nova tarefa" />
+          <button>
+            Criar <PlusCircle size={16} />
+          </button>
+        </form>
+      </main>
+    </>
+  );
 }
