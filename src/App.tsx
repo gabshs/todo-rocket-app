a19d9e4f79e831d5df5dbd575ca7acc5254782a0
Gabriel Henrique <gabriel.henrique@warren.com.br>
2023-01-30 09:14:29 -0300

    feat: allow to create task

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,42 @@ import Logo from "./assets/logo.svg";
 import styles from "./App.module.css";
 import { Task } from "./components/Task";
 import { ITask } from "./types/ITask";
-
-const tasks: ITask[] = [
-  {
-    content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur minus consequatur aut ut",
-    isCompleted: false,
-  },
-  {
-    content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur minus consequatur aut ut",
-    isCompleted: false,
-  },
-  {
-    content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur minus consequatur aut ut",
-    isCompleted: false,
-  },
-];
+import { ChangeEvent, FormEvent, useState } from "react";
 
 export function App() {
+  const [tasks, setTasks] = useState([] as ITask[]);
+  const [content, setContent] = useState("");
+
+  function handleCreateTask(event: FormEvent) {
+    event?.preventDefault();
+
+    const task: ITask = {
+      content,
+      isCompleted: false,
+    };
+
+    setTasks([...tasks, task]);
+
+    setContent("");
+  }
+
   return (
     <>
       <header className={styles.header}>
         <img src={Logo} alt="todo logo" />
       </header>
       <main className={styles.container}>
-        <form className={styles.taskForm}>
-          <input type="text" placeholder="Adicione uma nova tarefa" />
-          <button>
+        <form className={styles.taskForm} onSubmit={handleCreateTask}>
+          <input
+            type="text"
+            name="content"
+            onChange={(event: ChangeEvent<HTMLInputElement>) => {
+              setContent(event.target.value);
+            }}
+            value={content}
+            placeholder="Adicione uma nova tarefa"
+          />
+          <button type="submit">
             Criar <PlusCircle size={16} />
           </button>
         </form>
@@ -37,15 +47,15 @@ export function App() {
           <div className={styles.tasksInfo}>
             <div>
               <h3>Tarefas criadas</h3>
-              <span>0</span>
+              <span>{tasks.length}</span>
             </div>
             <div>
               <h3>Concluídas</h3>
-              <span>1 de 2</span>
+              <span>0</span>
             </div>
           </div>
           {tasks.map((task) => (
-            <Task task={task} />
+            <Task task={task} key={task.content} />
           ))}
         </div>
       </main>
